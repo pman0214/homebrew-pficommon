fcb074909aa5bc5a4aba9a18e57e89bc8ba0e3cf
Shigemi ISHIDA <ishida+devel@f.ait.kyushu-u.ac.jp>
2016-12-02 16:59:44 +0900

    Use SHA-256.

--- pficommon.rb
+++ pficommon.rb
@@ -4,7 +4,7 @@ class Pficommon < Formula
   url 'https://github.com/gwtnb/pficommon/tarball/mavericks-20140813'
   head 'https://github.com/pfi/pficommon.git'
   homepage 'http://pfi.github.io/pficommon/'
-  sha1 'b3be97c0a87ae8cf79228d751e4a36bdee4013af'
+  sha256 'f4132eb8ce8c5635c869d178b9bae4f9a1b2e15f5d77193a59010a035531a923'
   version 'mavericks-20140813'
 
   depends_on 'msgpack059'
